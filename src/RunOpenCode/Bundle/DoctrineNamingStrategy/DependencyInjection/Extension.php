6eb5bf9070cd8011aa3816567d86a5ef1267e4e0
Nikola Svitlica a.k.a. TheCelavi <thecelavi@mac.com>
2015-11-29 22:43:56 +0100

    Added missing license info on top of the files.

--- src/RunOpenCode/Bundle/DoctrineNamingStrategy/DependencyInjection/Extension.php
+++ src/RunOpenCode/Bundle/DoctrineNamingStrategy/DependencyInjection/Extension.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * This file is part of the Doctrine Naming Strategy Bundle, an RunOpenCode project.
+ *
+ * (c) 2015 RunOpenCode
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace RunOpenCode\Bundle\DoctrineNamingStrategy\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
