56b60f77fd43830a2dba63c806d546d1278549b6
Nikola Svitlica a.k.a. TheCelavi <thecelavi@mac.com>
2015-12-18 10:43:21 +0100

    Added typo fix

--- src/RunOpenCode/Bundle/DoctrineNamingStrategy/DependencyInjection/Configuration.php
+++ src/RunOpenCode/Bundle/DoctrineNamingStrategy/DependencyInjection/Configuration.php
@@ -23,7 +23,7 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
 
-        $rootNode = $treeBuilder->root('run_open_code_doctrine_naming_strategy');
+        $rootNode = $treeBuilder->root('underscored_bundle_prefix');
 
         $rootNode
             ->children()
